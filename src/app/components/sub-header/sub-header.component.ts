fe1cae4f005c9117a416529186383a115fcc5e9d
garrett <garrettculos@gmail.com>
2018-09-23 11:26:50 -0700

    Update temporary front page

--- src/app/components/sub-header/sub-header.component.ts
+++ src/app/components/sub-header/sub-header.component.ts
@@ -8,17 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class SubHeaderComponent {
     public people = [
         {
+            image: 'https://graph.facebook.com/10158699463730057/picture?height=256&width=256',
             name: 'Garrett',
-            totalDistance: 78,
+            totalDistance: 131.5,
             totalDistanceUnits: 'km',
-            weeksDistance: 10,
+            weeksDistance: 23.4,
             weeksDistanceUnits: 'km'
         },
         {
+            image: 'https://graph.facebook.com/10155703688449779/picture?height=256&width=256',
             name: 'Tony',
-            totalDistance: 78,
+            totalDistance: 201.0,
             totalDistanceUnits: 'km',
-            weeksDistance: 10,
+            weeksDistance: 30.7,
             weeksDistanceUnits: 'km'
         }
     ];
